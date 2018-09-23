acc33fb98af734918104c94cf270fdd1287f5075
f4tp <mr_treadwell@live.co.uk>
2018-09-23 13:16:43 +0100

    Initial Commit - 30 minutes in

---
+++ VidlySept2018/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlySept2018.Models;
+
+namespace VidlySept2018.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var ranMovie = new Movie(){Name = "Shrek!", Id = 1};
+            
+            //return View(ranMovie); //passes Movie Model to view
+            //return Content("Hello World"); //overwrites content
+            return HttpNotFound("not found error here");
+        }
+    }
+}
---
+++ VidlySept2018/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlySept2018.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public Movie()
+        {
+                
+        }
+    }
+}
